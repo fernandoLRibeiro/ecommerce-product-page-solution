7f517db6efc45865d8383dd29e81ed0b0e1a5da4
Fernando L. Ribeiro <fernandoribeiro@protonmail.com>
2022-08-05 01:22:12 -0300

    Finish basic desktop cart styling

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "../styles/Cart.module.css";
 
 function Cart() {
+  const [openCart, setOpenCart] = useState(false);
+
   return (
     <div className={styles.Cart}>
-      <img
-        className={styles.icon}
-        src="./assets/images/icon-cart.svg"
-        alt="cart"
-      />
+      <div
+        className={styles.container}
+        onClick={() => {
+          setOpenCart(!openCart);
+        }}
+      >
+        <div className={styles.badge}>3</div>
+        <img
+          className={styles.icon}
+          src="./assets/images/icon-cart.svg"
+          alt="cart"
+        />
+      </div>
+      {openCart && (
+        <div className={styles.openCart}>
+          <header className={styles.header}>
+            <h5 className={styles.heading}>Cart</h5>
+          </header>
+          <div className={styles.secondHalf}>
+            <div className={styles.product}>
+              <img
+                className={styles.productThumbnail}
+                src="./assets/images/image-product-1-thumbnail.jpg"
+                alt="product thumbnail"
+              />
+              <div className={styles.productInfo}>
+                <p className={styles.productName}>
+                  Fall Limited Edition Sneakers
+                </p>
+                <div className={styles.priceCluster}>
+                  <p className={styles.relativePrice}>$125.00 x 3</p>
+                  <p className={styles.totalPrice}>$375.00</p>
+                </div>
+              </div>
+              <img
+                className={styles.deleteIcon}
+                src="./assets/images/icon-delete.svg"
+                alt="delete"
+              />
+            </div>
+            <button className={styles.button}>Checkout</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
